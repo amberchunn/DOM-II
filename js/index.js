7e5d89ef28700555ebf7b3bee678005191470106
Amber Chunn <a.77@me.com>
2020-02-18 16:50:40 -0800

    Added preventDefault for navigation items

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+
+// Prevent navigation from default refresh behavior
+
+// const eventHandler = event => {
+// 	stopPropagation();
+// 	console.log('Help!');
+// };
+
+// let stopRefresh = navLinks.forEach(item => {
+// 	eventHandler(item);
+// });
+// const navLinks = document.querySelectorAll('nav a');
+let stopNavRefresh = document.querySelectorAll('nav a').forEach(item => {
+	item.addEventListener('click', event => {
+		// event.preventDefault();
+		console.log(`clicked ${item}`);
+	});
+});
+
+// navLinks.addEventListener('click', event => {
+// 	navLinks.innerHTML = `Click count: ${event.detail}`;
+// });
